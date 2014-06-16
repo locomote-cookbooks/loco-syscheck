3ee8f82c82ffe0d9a0babdb188899f0f87b0cd32
chris <chris@locomote.com.au>
2014-06-16 14:57:05 +1000

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devs@locomote.com'
 license          'BSD'
 description      'A Chef cookbook to install and run loco specific syschecks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 
 %w{ ubuntu debian redhat centos gentoo}.each { |os| supports os }
 
